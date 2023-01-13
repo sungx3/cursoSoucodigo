b820ae505d51b10f35134c0fe8cafea54446e472
sungx3 <119907831+sungx3@users.noreply.github.com>
2023-01-13 20:31:12 -0300

    Add files via upload
    Semaforo - Realizado em aula

---
+++ Semaforo/js/semaforo.js
@@ -0,0 +1,25 @@
+const img = document.getElementById('img');
+const buttons = document.getElementById('buttons');
+let colorIndex = 0;
+let intervalId = null;
+const trafficLight = (event) =>{
+    stopAutomatic(); // Função a ser criada
+    turnOn[event.target.id]();
+}
+const nextIndex = () => colorIndex = colorIndex < 2 ? ++ colorIndex: 0;
+const changeColor = () => {
+    const colors = ['red', 'yellow', 'green']
+    const color = colors[colorIndex];
+    turnOn[color]();
+    nextIndex();
+}
+const stopAutomatic = () => {
+    clearInterval(intervalId);
+}
+const turnOn = {
+    'red': () => img.src = './vermelho.png',
+    'yellow': () => img.src = './amarelo.png',
+    'green': () => img.src = './verde.png',
+    'automatic': () => intervalId = setInterval(changeColor, 30),
+}
+buttons.addEventListener('click', trafficLight)
